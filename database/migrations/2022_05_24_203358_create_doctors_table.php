da375bc37e2bb014720c3c642f3344043ada9503
Kirill Tremaskin <tremaskinld@yandex.ru>
2022-05-25 01:30:25 +0300

    Migrations

---
+++ database/migrations/2022_05_24_203358_create_doctors_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->id();
+            $table->integer('polyclinic_id');
+            $table->integer('user_id');
+            $table->integer('spec_id');
+            $table->string('additional_specialization');
+            $table->string('work_phone');
+            $table->string('working_hours');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('doctors');
+    }
+};
--- database/migrations/2022_05_24_204248_create_appointment_items_table.php
+++ database/migrations/2022_05_24_204248_create_appointment_items_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('appointment_items', function (Blueprint $table) {
+            $table->id();
+            $table->integer('app_id');
+            $table->integer('doc_service_id');
+            $table->string('description');
+            $table->float('price');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('appointment_items');
     }
 };
